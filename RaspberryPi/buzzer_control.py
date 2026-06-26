20f47eae3d337be1767213536768c65e55408b14
Mizzatul_ <muhammadizzatulislam78@gmail.com>
2026-06-26 17:34:52 +0700

    change buzzer pin from 16 to 5

--- RaspberryPi/buzzer_control.py
+++ RaspberryPi/buzzer_control.py
@@ -5,7 +5,7 @@ import time
 # Konfigurasi Pin
 # ==========================================
 # Pastikan menggunakan pin yang sama seperti sebelumnya
-PIN_BUZZER = 16
+PIN_BUZZER = 5
 
 def setup():
     # Menonaktifkan peringatan
